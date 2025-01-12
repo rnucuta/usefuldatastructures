b28df614403fda74f51405a5f892aaf8373e5a56
Raymond Nucuta <rnucuta@gmail.com>
2025-01-12 18:29:01 -0500

    fix paths in cms test for github action

--- python/tests/test_cms.py
+++ python/tests/test_cms.py
@@ -34,7 +34,7 @@ class TestCMS(unittest.TestCase):
                 process_stream(a=test[0], b=test[1], p=test[2], w=test[3], stream=iter(test[4])), ans)
 
     def test_complex(self):
-        with open("./inputs/CMS_inputs.json", "rt") as f:
+        with open("./tests/inputs/CMS_inputs.json", "rt") as f:
             test_problems = json.load(f)
 
         for test in test_problems:
